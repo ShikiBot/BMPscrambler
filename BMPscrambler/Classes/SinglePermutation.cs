4774461da76130f450c11797d1727a0e2c672c20
bot <xxx-yyy-2014@inbox.ru>
2021-04-02 13:02:16 +0500

    + шифр блочной одинарной перестановки

---
+++ BMPscrambler/Classes/SinglePermutation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMPscrambler.Classes
+{
+    class SinglePermutation 
+    {
+        public Bitmap Code(Bitmap image, int[] key)
+        {
+            Bitmap Cimage = new Bitmap(image);
+            for (int i = 0; i < image.Width * image.Height / key.Length; i++)
+                for (int j = 0; j < key.Length; j++)
+                    Cimage.SetPixel((i*key.Length + j) / Cimage.Width, (i * key.Length + j) % Cimage.Width, image.GetPixel((i * key.Length + key[j]) / image.Width, (i * key.Length + key[j]) % image.Width));
+            return Cimage;
+        }
+
+        public Bitmap Decode(Bitmap image, int[] key)
+        {
+            Bitmap Cimage = new Bitmap(image);
+            for (int i = 0; i < image.Width * image.Height / key.Length; i++)
+                for (int j = 0; j < key.Length; j++)
+                    Cimage.SetPixel((i * key.Length + key[j]) / image.Width, (i * key.Length + key[j]) % image.Width, image.GetPixel((i * key.Length + j) / Cimage.Width, (i * key.Length + j) % Cimage.Width));
+            return Cimage;
+        }
+
+        public int[] genKey(int count)
+        {
+            int[] key = new int[count];
+            for (int i = 0; i < count; i++)
+                key[i] = i;
+            key = shuffle(key);
+            return key;
+        }
+
+        private int[] shuffle(int[] array)
+        {
+            Random rng = new Random();
+            int n = array.Length;
+            while (n > 1)
+            {
+                int k = rng.Next(n);
+                n--;
+                int temp = array[n];
+                array[n] = array[k];
+                array[k] = temp;
+            }
+            return array;
+        }
+    }
+}
--- BMPscrambler/Form1.cs
+++ BMPscrambler/Form1.cs
@@ -4,7 +4,7 @@ using System.Windows;
 using System.Windows.Forms;
 using BMPscrambler.Classes;
 using System.Linq;
-
+using System.Collections.Generic;
 
 namespace BMPscrambler
 {
@@ -14,28 +14,19 @@ namespace BMPscrambler
         ImageInfo cypheredImage;
         readonly CypherImage cImage;
         HillCipher hillCipher = new HillCipher();
-        int k = 2;
+        SinglePermutation perm1 = new SinglePermutation();
+        int k = 3;
         long abc = 4294967296; //(int)Math.Pow(256, 4);
         Matrix mess, ekey, dkey, cim, im;
+        int[] key;
 
         public Form1()
         {
             InitializeComponent();
             cImage = new CypherImage();
             originalImage = new ImageInfo(Properties.Resources.GokuSSBlue);
-            cypheredImage = new ImageInfo(Properties.Resources.GokuSSBlue);
-
-            //int x, y;
-            //MessageBox.Show(hillCipher.Gcd(379, 37, out x, out y) + " " + x + " " + y);
-
-            /*mess = new Matrix(CypherImage.ToLongArray(originalImage.Image), k);
-            var keys = hillCipher.GenKey(k, abc);
-            ekey = keys[KeyType.ENCRYPT];
-            dkey = keys[KeyType.DECRYPT];
-            //key = new Matrix(new int[,] { { 0, 12, 29 }, { 16, 9, 14 }, { 9, 8, 13 } });
-            cim = hillCipher.Encrypt(mess, ekey, k, abc);            
-            cypheredImage = new ImageInfo(HillCipher.FromLongArray(cim.Array.Cast<long>().ToArray()));*/
-        }
+            cypheredImage = new ImageInfo(Properties.Resources.GokuSSBlue);            
+        }        
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -46,12 +37,19 @@ namespace BMPscrambler
 
         private void button4_Click(object sender, EventArgs e)
         {
-            mess = new Matrix(CypherImage.ToLongArray(originalImage.Image), k);
+            //hill
+            /*mess = new Matrix(CypherImage.ToLongArray(originalImage.Image), k);
             var keys = hillCipher.GenKey(k, abc);
             ekey = keys[KeyType.ENCRYPT];
             dkey = keys[KeyType.DECRYPT];
             cim = hillCipher.Encrypt(mess, ekey, k, abc);
-            cypheredImage = new ImageInfo(HillCipher.FromLongArray(cim.Array.Cast<long>().ToArray()));
+            cypheredImage = new ImageInfo(HillCipher.FromLongArray(cim.Array.Cast<long>().ToArray()));*/
+
+            //1perutation
+            key = perm1.genKey(k);
+            cypheredImage = new ImageInfo(perm1.Code(originalImage.Image, key));
+
+
             label1.Text = cypheredImage.H + " " + k;
             pictureBox2.Image = cypheredImage.Image;
         }
@@ -59,13 +57,6 @@ namespace BMPscrambler
         private void button1_Click(object sender, EventArgs e)
         {
             k++;
-            /*mess = new Matrix(CypherImage.ToLongArray(originalImage.Image), k);
-            var keys = hillCipher.GenKey(k, abc);
-            ekey = keys[KeyType.ENCRYPT];
-            dkey = keys[KeyType.DECRYPT];
-            cim = hillCipher.Encrypt(mess, ekey, k, abc);
-            cypheredImage = new ImageInfo(HillCipher.FromLongArray(cim.Array.Cast<long>().ToArray()));
-            pictureBox2.Image = cypheredImage.Image;*/
             label1.Text = cypheredImage.H + " " + k;
             
         }
@@ -73,33 +64,20 @@ namespace BMPscrambler
         private void button2_Click(object sender, EventArgs e)
         {
             k = k > 2 ? k-=1 : k;
-            /*mess = new Matrix(CypherImage.ToLongArray(originalImage.Image), k);
-            var keys = hillCipher.GenKey(k, abc);
-            ekey = keys[KeyType.ENCRYPT];
-            dkey = keys[KeyType.DECRYPT];
-            cim = hillCipher.Encrypt(mess, ekey, k, abc);
-            cypheredImage = new ImageInfo(HillCipher.FromLongArray(cim.Array.Cast<long>().ToArray()));*/
             label1.Text = cypheredImage.H + " " + k;
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            /*Matrix message = new Matrix(new int[,] { { 25, 9, 21 }, { 17, 35, 35 } });
-            //MessageBox.Show("message\n" + message.ToString());
-            Matrix kkey = new Matrix(new int[,] { { 0, 12, 29 }, { 16, 9, 14 }, { 9, 8, 13 } });
-            //MessageBox.Show("key\n" + kkey.ToString());
-            Matrix cmessage = hillCipher.Encrypt(message, kkey, 3, 37);
-            //MessageBox.Show("cmessage\n" + cmessage.ToString());
-            //MessageBox.Show("det\n" + kkey.Determinant());
-            Matrix revkkey = kkey.InverseByMod(33, 37);
-            //MessageBox.Show("revkkey\n" + revkkey.ToString());
-            Matrix matrixD = (kkey * revkkey) % 37;
-            //MessageBox.Show(matrixD.ToString());
-            Matrix dmessage = hillCipher.Decrypt(cmessage, kkey, k, 37);
-            //MessageBox.Show(dmessage.ToString());*/
+            //hill
+            /*im = hillCipher.Decrypt(mess, dkey, k, abc);            
+            cypheredImage = new ImageInfo(HillCipher.FromLongArray(im.Array.Cast<long>().ToArray()));*/
 
-            im = hillCipher.Decrypt(mess, dkey, k, abc);            
-            cypheredImage = new ImageInfo(HillCipher.FromLongArray(im.Array.Cast<long>().ToArray()));
+
+            //1perutation
+            cypheredImage = new ImageInfo(perm1.Decode(cypheredImage.Image, key));
+
+
             label1.Text = cypheredImage.H + " " + k;
             pictureBox2.Image = cypheredImage.Image;
         }
